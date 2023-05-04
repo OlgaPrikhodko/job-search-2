e8f7c973c25a1e030d7a2b8e2734f325611f8458
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-05-04 16:19:36 +0300

    add MainNav test

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -4,7 +4,7 @@
       <div
         class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-grey-1 px-8"
       >
-        <a href="/" class="flex h-full items-center text-xl">Bobo Careers</a>
+        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
       </div>
     </div>
   </header>
@@ -13,5 +13,10 @@
 <script>
 export default {
   name: "MainNav",
+  data() {
+    return {
+      company: "Bobo Careers",
+    };
+  },
 };
 </script>
