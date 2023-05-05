0085e20e61307f04646a88c47f3e71f3ac4b9f80
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-05-05 16:24:12 +0300

    add test for menu items for navigation

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -8,4 +8,18 @@ describe("MainNav", () => {
     const companyName = screen.getByText("Bobo Careers");
     expect(companyName).toBeInTheDocument();
   });
+
+  it("displays menu items for navigation", () => {
+    render(MainNav);
+    const navMenuItems = screen.getAllByRole("listitem");
+    const navMenuTexts = navMenuItems.map((menuItem) => menuItem.textContent);
+    expect(navMenuTexts).toEqual([
+      "Teams",
+      "Location",
+      "Life at Bobo Corp",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
+  });
 });
