0a78c7371148db75ca79e935944e93ad4f65143e
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-05-08 09:17:52 +0300

    add content to TheSubnav

--- src/components/TheSubnav.vue
+++ src/components/TheSubnav.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="h-16 w-full border-b border-solid border-brand-grey-1 bg-white">
-    <div class="flex h-full items-center px-8">This is a subnav</div>
+    <div class="flex h-full items-center px-8">
+      <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
+      <span><span class="text-brand-green-1">1653</span> jobs marched</span>
+    </div>
   </div>
 </template>
 
