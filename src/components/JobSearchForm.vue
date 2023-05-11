2262aea17483b87c6e57f3b5c37acaf0f19ea0e5
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-05-11 16:16:43 +0300

    add form structure and styles

--- src/components/JobSearchForm.vue
+++ src/components/JobSearchForm.vue
@@ -1,9 +1,44 @@
 <template>
-  <form>JobSearchForm</form>
+  <form
+    class="flex h-12 w-full items-center rounded-3xl border border-solid border-brand-gray-3"
+  >
+    <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
+
+    <div class="flex-no-wrap flex h-full flex-1 text-base font-light">
+      <div class="relative h-full flex-1 items-center pr-3">
+        <label class="absolute -top-10 left-0" for="">Role</label>
+        <input
+          type="text"
+          placeholder="Software ingeneer"
+          class="w-full text-lg font-normal focus:outline-none"
+        />
+      </div>
+
+      <span
+        class="flex h-full items-center border-l border-r border-brand-gray-3 bg-brand-gray-2 px-3"
+      >
+        in
+      </span>
+
+      <div class="relative h-full flex-1 items-center pl-3">
+        <label class="absolute -top-10 left-0" for="">Where</label>
+        <input
+          type="text"
+          placeholder="Los Angeles"
+          class="w-full text-lg font-normal focus:outline-none"
+        />
+      </div>
+    </div>
+
+    <ActionButton text="Search" type="secondary" class="rounded-r-3xl" />
+  </form>
 </template>
 
 <script>
+import ActionButton from "@/components/ActionButton.vue";
+
 export default {
   name: "JobSearchForm",
+  components: { ActionButton },
 };
 </script>
