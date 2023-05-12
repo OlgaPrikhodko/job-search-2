18d155594e01d0385d1db44a4d7fd75bddee73f0
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-05-12 16:37:17 +0300

    add MainNav to App

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
+  <MainNav />
+
   <RouterView />
 </template>
 
 <script>
+import MainNav from "@/components/Navigation/MainNav.vue";
+
 export default {
   name: "App",
+  components: { MainNav },
 };
 </script>
