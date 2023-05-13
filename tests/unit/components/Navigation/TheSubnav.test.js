62902962b7927fbd6c28e660d09533d968b30bea
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-05-13 06:59:36 +0300

    ref: add facrory func for component rendering

--- tests/unit/components/Navigation/TheSubnav.test.js
+++ tests/unit/components/Navigation/TheSubnav.test.js
@@ -3,24 +3,23 @@ import { render, screen } from "@testing-library/vue";
 import TheSubnav from "@/components/Navigation/TheSubnav.vue";
 
 describe("TheSubnav", () => {
+  const renderTheSubnav = (routeName) => {
+    render(TheSubnav, {
+      global: {
+        mocks: {
+          $route: { name: routeName },
+        },
+        stubs: {
+          FontAwesomeIcon: true,
+        },
+      },
+    });
+  };
+
   describe("when user is on a job page", () => {
     it("displays job count", () => {
-      const $route = { name: "JobResults" };
-      render(TheSubnav, {
-        global: {
-          mocks: {
-            $route,
-          },
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-        data() {
-          return {
-            onJobResultsPage: true,
-          };
-        },
-      });
+      const routeName = "JobResults";
+      renderTheSubnav(routeName);
 
       const jobCount = screen.getByText("1653");
 
@@ -30,23 +29,8 @@ describe("TheSubnav", () => {
 
   describe("when user is not on a job page", () => {
     it("does not display job count", () => {
-      const $route = { name: "Home" };
-
-      render(TheSubnav, {
-        global: {
-          mocks: {
-            $route,
-          },
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-        data() {
-          return {
-            onJobResultsPage: false,
-          };
-        },
-      });
+      const routeName = "Home";
+      renderTheSubnav(routeName);
 
       const jobCount = screen.queryByText("1653");
 
