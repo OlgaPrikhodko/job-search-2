551799e2e92a40d6a907545adf9805d02eacef65
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-05-18 16:16:18 +0300

    add JobFiltersSideBar

---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -0,0 +1,35 @@
+<template>
+  <CollapsibleAccordion header="Organizations">
+    <div class="mt-5">
+      <fieldset>
+        <ul class="flex flex-row flex-wrap">
+          <li class="h-8 w-1/2">
+            <input id="VueTube" type="checkbox" class="mr-3" />
+            <label for="VueTube">VueTube</label>
+          </li>
+          <li class="h-8 w-1/2">
+            <input id="Between Vue" type="checkbox" class="mr-3" />
+            <label for="Between Vue">Between Vue</label>
+          </li>
+          <li class="h-8 w-1/2">
+            <input id="VueTube" type="checkbox" class="mr-3" />
+            <label for="VueTube">VueTube</label>
+          </li>
+          <li class="h-8 w-1/2">
+            <input id="VueTube" type="checkbox" class="mr-3" />
+            <label for="VueTube">VueTube</label>
+          </li>
+        </ul>
+      </fieldset>
+    </div>
+  </CollapsibleAccordion>
+</template>
+
+<script>
+import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
+
+export default {
+  name: "JobFiltersSidebarOrganizations",
+  components: { CollapsibleAccordion },
+};
+</script>
--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import JobFiltersSidebar from "@/components/JobResults/JobFilterSidebar/JobFiltersSidebar.vue";
+import JobFiltersSidebar from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue";
 import JobListings from "@/components/JobResults/JobListings.vue";
 
 export default {
