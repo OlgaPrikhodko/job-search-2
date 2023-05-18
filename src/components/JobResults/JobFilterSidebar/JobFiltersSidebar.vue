f38ed85182bf616a8c0c7ee6b4b1468e68e92a48
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-05-18 13:59:28 +0300

    add CollapsibleAccordion

--- src/components/JobResults/JobFilterSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFilterSidebar/JobFiltersSidebar.vue
@@ -9,14 +9,41 @@
           <ActionButton text="Clear Filters" type="secondary"></ActionButton>
         </div>
       </div>
+
+      <CollapsibleAccordion header="Organizations">
+        <div class="mt-5">
+          <fieldset>
+            <ul class="flex flex-row flex-wrap">
+              <li class="h-8 w-1/2">
+                <input id="VueTube" type="checkbox" class="mr-3" />
+                <label for="VueTube">VueTube</label>
+              </li>
+              <li class="h-8 w-1/2">
+                <input id="Between Vue" type="checkbox" class="mr-3" />
+                <label for="Between Vue">Between Vue</label>
+              </li>
+              <li class="h-8 w-1/2">
+                <input id="VueTube" type="checkbox" class="mr-3" />
+                <label for="VueTube">VueTube</label>
+              </li>
+              <li class="h-8 w-1/2">
+                <input id="VueTube" type="checkbox" class="mr-3" />
+                <label for="VueTube">VueTube</label>
+              </li>
+            </ul>
+          </fieldset>
+        </div>
+      </CollapsibleAccordion>
     </section>
   </div>
 </template>
 
 <script>
 import ActionButton from "@/components/Shared/ActionButton.vue";
+import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
+
 export default {
   name: "JobFiltersSidebar",
-  components: { ActionButton },
+  components: { ActionButton, CollapsibleAccordion },
 };
 </script>
