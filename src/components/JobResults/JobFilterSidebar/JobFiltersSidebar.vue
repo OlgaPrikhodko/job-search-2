9a97034cbfbec19526158cf5c07f63408c02fd75
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-05-18 12:25:06 +0300

    add top of filters components

--- src/components/JobResults/JobFilterSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFilterSidebar/JobFiltersSidebar.vue
@@ -2,12 +2,21 @@
   <div
     class="border-brand-gray-1 flex w-96 flex-col border-r border-solid bg-white p-4"
   >
-    Job Filters Sidebar
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
+        <div class="flex items-center text-sm">
+          <ActionButton text="Clear Filters" type="secondary"></ActionButton>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import ActionButton from "@/components/Shared/ActionButton.vue";
 export default {
   name: "JobFiltersSidebar",
+  components: { ActionButton },
 };
 </script>
