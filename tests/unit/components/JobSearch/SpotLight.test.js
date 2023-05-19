d1e9f807347a59ca7ed66de58cf04852b5681472
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-05-19 14:42:48 +0300

    add spotlight tests

---
+++ tests/unit/components/JobSearch/SpotLight.test.js
@@ -0,0 +1,67 @@
+import { render, screen } from "@testing-library/vue";
+import axios from "axios";
+
+import SpotLight from "@/components/JobSearch/SpotLight.vue";
+
+vi.mock("axios");
+
+describe("SpotLight", () => {
+  const mockSpotlightResponse = (spotlight) => {
+    axios.get.mockResolvedValue({
+      data: [
+        {
+          id: 1,
+          title: "Title",
+          description: "Description",
+          img: "Image",
+          ...spotlight,
+        },
+      ],
+    });
+  };
+
+  it("provides image to parent component", async () => {
+    const spotlight = { img: "Some image" };
+    mockSpotlightResponse(spotlight);
+
+    render(SpotLight, {
+      slots: {
+        default: `<template #default="slotProps">
+           <h1>{{ slotProps.img }}</h1>
+        </template>`,
+      },
+    });
+    const text = await screen.findByText("Some image");
+    expect(text).toBeInTheDocument();
+  });
+
+  it("provides title to parent component", async () => {
+    const spotlight = { title: "Some title" };
+    mockSpotlightResponse(spotlight);
+
+    render(SpotLight, {
+      slots: {
+        default: `<template #default="slotProps">
+           <h1>{{ slotProps.title }}</h1>
+        </template>`,
+      },
+    });
+    const text = await screen.findByText("Some title");
+    expect(text).toBeInTheDocument();
+  });
+
+  it("provides description to parent component", async () => {
+    const spotlight = { description: "Some description" };
+    mockSpotlightResponse(spotlight);
+
+    render(SpotLight, {
+      slots: {
+        default: `<template #default="slotProps">
+           <h1>{{ slotProps.description }}</h1>
+        </template>`,
+      },
+    });
+    const text = await screen.findByText("Some description");
+    expect(text).toBeInTheDocument();
+  });
+});
