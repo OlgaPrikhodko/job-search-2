dd38d5b0b8c08ce42f50dc15f3796c030e8c8c30
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-05-20 07:10:28 +0300

    save selectedOrganizations in user store

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -8,7 +8,14 @@
             :key="organization"
             class="h-8 w-1/2"
           >
-            <input :id="organization" type="checkbox" class="mr-3" />
+            <input
+              :id="organization"
+              v-model="selectedOrganizations"
+              :value="organization"
+              type="checkbox"
+              class="mr-3"
+              @change="selectOrganization"
+            />
             <label :for="organization">{{ organization }}</label>
           </li>
         </ul>
@@ -18,17 +25,29 @@
 </template>
 
 <script>
-import { mapState } from "pinia";
+import { mapActions, mapState } from "pinia";
 
 import { useJobsStore, UNIQUE_ORGANIZATIONS } from "@/stores/jobs";
+import { useUserStore, ADD_SELECTED_ORGANIZATIONS } from "@/stores/user";
 
 import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
 
 export default {
   name: "JobFiltersSidebarOrganizations",
   components: { CollapsibleAccordion },
+  data() {
+    return {
+      selectedOrganizations: [],
+    };
+  },
   computed: {
     ...mapState(useJobsStore, [UNIQUE_ORGANIZATIONS]),
+  },
+  methods: {
+    ...mapActions(useUserStore, [ADD_SELECTED_ORGANIZATIONS]),
+    selectOrganization() {
+      this.ADD_SELECTED_ORGANIZATIONS(this.selectedOrganizations);
+    },
   },
 };
 </script>
--- tests/unit/stores/user.test.js
+++ tests/unit/stores/user.test.js
@@ -11,6 +11,11 @@ describe("state", () => {
     const store = useUserStore();
     expect(store.isLoggedIn).toBeFalsy();
   });
+
+  it("stores organizations that the user would like to filter jobs by", () => {
+    const store = useUserStore();
+    expect(store.selectedOrganizations).toEqual([]);
+  });
 });
 
 describe("actions", () => {
@@ -18,11 +23,19 @@ describe("actions", () => {
     setActivePinia(createPinia());
   });
 
-  describe("logginUser", () => {
+  describe("loginUser", () => {
     it("logs user in", () => {
       const store = useUserStore();
       store.loginUser();
       expect(store.isLoggedIn).toBeTruthy();
+    });
+  });
+
+  describe("ADD_SELECTED_ORGANIZATIONS", () => {
+    it("updates organizations the user has chosen to filter jobs", () => {
+      const store = useUserStore();
+      store.ADD_SELECTED_ORGANIZATIONS(["Google", "Amazon"]);
+      expect(store.selectedOrganizations).toEqual(["Google", "Amazon"]);
     });
   });
 });
