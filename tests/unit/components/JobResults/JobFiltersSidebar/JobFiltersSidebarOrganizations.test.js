09b081238096054edd09bd49029aee836d21027b
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-05-21 05:55:09 +0300

    test JobFiltersSidebarOrganizations

--- tests/unit/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.test.js
+++ tests/unit/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.test.js
@@ -3,14 +3,15 @@ import userEvent from "@testing-library/user-event";
 import { createTestingPinia } from "@pinia/testing";
 
 import { useJobsStore } from "@/stores/jobs";
+import { useUserStore } from "@/stores/user";
 import JobFiltersSidebarOrganizations from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue";
 
 describe("JobFiltersSidebarOrganizations", () => {
-  it("renders unique list of organizations from jobs", async () => {
+  const renderJobFiltersSidebarOrganizations = () => {
     const pinia = createTestingPinia();
 
+    const userStore = useUserStore();
     const jobStore = useJobsStore();
-    jobStore.UNIQUE_ORGANIZATIONS = new Set(["Google", "Amazon"]);
 
     render(JobFiltersSidebarOrganizations, {
       global: {
@@ -19,6 +20,14 @@ describe("JobFiltersSidebarOrganizations", () => {
       },
     });
 
+    return { jobStore, userStore };
+  };
+
+  it("renders unique list of organizations from jobs", async () => {
+    const { jobStore } = renderJobFiltersSidebarOrganizations();
+
+    jobStore.UNIQUE_ORGANIZATIONS = new Set(["Google", "Amazon"]);
+
     const button = screen.getByRole("button", { name: /organizations/i });
     await userEvent.click(button);
 
@@ -26,5 +35,21 @@ describe("JobFiltersSidebarOrganizations", () => {
     const organizations = filterList.map((node) => node.textContent);
 
     expect(organizations).toEqual(["Google", "Amazon"]);
+  });
+
+  it("it communicates that user has selected checkbox for organizations", async () => {
+    const { jobStore, userStore } = renderJobFiltersSidebarOrganizations();
+
+    jobStore.UNIQUE_ORGANIZATIONS = new Set(["Google", "Amazon"]);
+
+    const button = screen.getByRole("button", { name: /organizations/i });
+    await userEvent.click(button);
+
+    const checkbox = screen.getByRole("checkbox", { name: /amazon/i });
+    await userEvent.click(checkbox);
+
+    expect(userStore.ADD_SELECTED_ORGANIZATIONS).toHaveBeenCalledWith([
+      "Amazon",
+    ]);
   });
 });
