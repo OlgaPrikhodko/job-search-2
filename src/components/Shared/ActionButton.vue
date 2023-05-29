650f53f456a37179627d31d1385111fdaf5c5a2b
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-05-29 11:56:21 +0300

    add types to ActionButton component

--- src/components/Shared/ActionButton.vue
+++ src/components/Shared/ActionButton.vue
@@ -4,7 +4,7 @@
   </button>
 </template>
 
-<script setup>
+<script lang="ts" setup>
 import { computed, toRefs } from "vue";
 
 const props = defineProps({
@@ -13,7 +13,7 @@ const props = defineProps({
     type: String,
     required: false,
     default: "primary",
-    validator(value) {
+    validator(value: string) {
       return ["primary", "secondary"].includes(value);
     },
   },
