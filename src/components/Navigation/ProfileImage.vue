143db7f8d000c9d3365681e422ed1b107e8caa1d
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-05-29 09:12:13 +0300

    add type annotation to ProfileImage component

--- src/components/Navigation/ProfileImage.vue
+++ src/components/Navigation/ProfileImage.vue
@@ -6,14 +6,7 @@
   />
 </template>
 
-<script>
-export default {
-  name: "ProfileImage",
-  data() {
-    return {
-      imageLink:
-        "https://st2.depositphotos.com/4063385/6717/v/380/depositphotos_67174477-stock-illustration-cat-logo.jpg",
-    };
-  },
-};
+<script lang="ts" setup>
+const imageLink =
+  "https://st2.depositphotos.com/4063385/6717/v/380/depositphotos_67174477-stock-illustration-cat-logo.jpg";
 </script>
